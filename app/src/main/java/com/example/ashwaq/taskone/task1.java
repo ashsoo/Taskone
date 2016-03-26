f9e1e9b7fbf5c992a3e61fc8a0aae36b94f8e415
ashsoo <ashwaq.soubai@yahoo.com>
2016-03-26 17:20:29 +0300

    first push

---
+++ app/src/main/java/com/example/ashwaq/taskone/task1.java
@@ -0,0 +1,61 @@
+package com.example.ashwaq.taskone;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+/* created by: ash
+* date 24 March 2016
+* a map show two different locations
+ */
+public class task1 extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task1);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in ksu and move the camera
+        //set as a default location
+
+        LatLng ksu = new LatLng(24.722575, 46.627601);
+        mMap.addMarker(new MarkerOptions().position(ksu).title("King Saud University")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ksu));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+        //here for PNU
+        LatLng pnu = new LatLng(24.722575, 46.627601);
+        mMap.addMarker(new MarkerOptions().position(pnu).title("Princess Noura University")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(pnu));
+    }
+}
